57fed9777b6ade7683c58f978b49560284bda725
Justin Pasko <jpasko1112@gmail.com>
2023-04-05 12:12:12 -0400

    Refactory the initialize method.

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -2,8 +2,8 @@ class Passenger
   attr_reader :name,
               :age
 
-  def initialize(name)
-    @name = name["name"]
-    @age = ["age"]
+  def initialize(details)
+    @name = details["name"]
+    @age = details["age"]
   end
 end
