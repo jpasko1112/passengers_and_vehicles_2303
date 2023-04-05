cb05ce80d6008bb280a42a2e3d7eee16819ba2f4
Justin Pasko <jpasko1112@gmail.com>
2023-04-05 12:43:43 -0400

    Add driver? test.

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 RSpec.describe Passenger do
   before(:each) do
-    # @passenger = Passenger.new({})
     @charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
     @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
   end
