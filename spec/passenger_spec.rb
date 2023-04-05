8b7d423e115c841d8f9c8c8f771b62935fd1d655
Justin Pasko <jpasko1112@gmail.com>
2023-04-05 17:20:19 -0400

    Clean up formatting.

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 RSpec.describe Passenger do
   before(:each) do
+    @passenger = Passenger.new({})
     @charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
     @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
   end
@@ -14,36 +15,27 @@ RSpec.describe Passenger do
 
   describe '#name' do
     it'returns the name' do
-
       expect(@charlie.name).to eq("Charlie")
-
-      expect(@taylor.name).to eq('Taylor')
     end
   end
 
   describe '#age' do
     it'returns the age' do
       expect(@charlie.age).to eq(18)
-      expect(@taylor.age).to eq(12)
     end
   end
 
   describe 'adult?' do
     it 'checks if it is an adult' do
-
       expect(@charlie.adult?).to eq(true)
-
       expect(@taylor.adult?).to eq(false)
     end
   end
 
   describe 'driver?' do
     it 'checks if it is a driver' do
-
       expect(@charlie.driver?).to eq(false)
-
       @charlie.drive
-
       expect(@charlie.driver?).to eq(true)
     end
   end
