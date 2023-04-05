81a5ac38550895ac5f377663590f08b59692ff53
Justin Pasko <jpasko1112@gmail.com>
2023-04-05 12:08:39 -0400

    Add a test for name.

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -2,27 +2,23 @@ require 'spec_helper'
 
 RSpec.describe Passenger do
   before(:each) do
-    @passenger = Passenger.new
+    # @passenger = Passenger.new({})
     @charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
     @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
   end
 
   describe '#initialize' do
    it 'can itialize a new Passenger' do
-    expect(@passenger).to be_a(Passenger)
+    expect(@charlie).to be_an_instance_of(Passenger)
    end
   end
 
   describe '#name' do
     it'returns the name' do
-     
-      @charlie.name
 
-      expect(@charlie.name).to eq('Charlie')
-
-      @taylor.name
+      expect(@charlie.name).to eq("Charlie")
 
       expect(@taylor.name).to eq('Taylor')
     end
   end
-  
+end
